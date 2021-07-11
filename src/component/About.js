268579df8951ccb5ca85aec9353385945c4fc14f
“Baplisca” <yokotako0327-goki@outlook.jp>
2021-07-11 16:02:54 +0900

    Moved the contents of Update on the About page to another component

--- src/component/About.js
+++ src/component/About.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import UpdateContent from './UpdateContent';
 import styled from 'styled-components';
 
 const AboutH2Wrapper = styled.h2`
@@ -7,16 +8,6 @@ const AboutH2Wrapper = styled.h2`
 `;
 const DisableListDot = styled.li`
   list-style-type: none;
-`;
-const HrWrapper = styled.hr`
-  border-width: 1px 0px 0px 0px;
-  border-style: solid;
-  border-color: #cccccc;
-  height: 1px;
-`;
-const UpdateHrWrapper = styled.div`
-  color: '#ffffff';
-  border-width: 2.5px 0 0 0;
 `;
 const ScrollWrapper = styled.div`
   height: 20em;
@@ -45,9 +36,6 @@ const ScrollOuterWrapper = styled.div`
   border-radius: 10px;
   border: none;
 `;
-const UpdateItemWrapper = styled.div`
-  margin-right: 50px;
-`;
 const ContentWrapper = styled.div`
   margin: 0px auto 0px auto;
   padding: 30px 0px 0px 0px;
@@ -67,89 +55,6 @@ const ContentWrapper = styled.div`
 `;
 //Class Component
 class About extends React.Component {
-  constructor(props, context) {
-    super(props, context);
-
-    let allItems = [
-      { value: 'TOEICスコア更新', date: '2020.6.8' },
-      { value: 'インターン更新', date: '2020.4.10' },
-      { value: 'Aboutページ更新', date: '2020.11.18' },
-      {
-        value: <a href="https://baplisca.herokuapp.com/">React適用に伴うサイト移動</a>,
-        date: '2020.11.1',
-      },
-      {
-        value: (
-          <a
-            href="https://www.youtube.com/channel/UCUEUc2teEFnYEXG-MtEncrA/playlists"
-            rel="noreferrer noopener"
-            target="_blank">
-            TadanoAction3DXGameのYoutube追加
-          </a>
-        ),
-        date: '2020.10.16',
-      },
-      {
-        value: (
-          <a
-            href="https://baplisca.hatenablog.com/entry/intern/nssol"
-            rel="noreferrer noopener"
-            target="_blank">
-            NSSOLインターンのブログリンク追加
-          </a>
-        ),
-        date: '2020.10.12',
-      },
-      {
-        value: (
-          <a
-            href="https://baplisca.hatenablog.com/entry/supporterz-1on1"
-            rel="noreferrer noopener"
-            target="_blank">
-            エンジニア1on1に参加しました
-          </a>
-        ),
-        date: '2020.10.5',
-      },
-      {
-        value: (
-          <a
-            href="https://baplisca.hatenablog.com/entry/supporterz-geekten"
-            rel="noreferrer noopener"
-            target="_blank">
-            技育展に登壇しました
-          </a>
-        ),
-        date: '2020.9.26',
-      },
-      { value: 'Youtube埋め込み・デザイン変更', date: '2020.9.13' },
-      { value: 'フッターに各種リンク追加・ナビゲーションバー変更', date: '2020.9.4' },
-      { value: 'インターンのブログリンク追加・フッター作成', date: '2020.9.3' },
-      { value: 'インターン項目・受賞追加', date: '2020.8.23' },
-      { value: '競プロライブラリ更新', date: '2020.7.22' },
-      { value: 'フォント変更 Google Map埋め込み', date: '2020.6.11' },
-      { value: 'スマホ対応, 画像圧縮 (PNG→JPEG)', date: '2020.5.29' },
-      { value: '競プロライブラリ追加', date: '2020.5.28' },
-      { value: 'HP作成', date: '2020.5.24' },
-    ];
-    this.state = { items: allItems };
-  }
-  RenderItems() {
-    return this.state.items.map((_item) => {
-      return (
-        <React.Fragment>
-          <UpdateItemWrapper>
-            <DisableListDot>{_item.date}</DisableListDot>
-            <DisableListDot>{_item.value}</DisableListDot>
-            <UpdateHrWrapper>
-              <HrWrapper />
-            </UpdateHrWrapper>
-          </UpdateItemWrapper>
-          <br />
-        </React.Fragment>
-      );
-    });
-  }
   render() {
     return (
       <React.Fragment>
@@ -324,8 +229,7 @@ class About extends React.Component {
           <ScrollOuterWrapper>
             <ScrollWrapper>
               <ul>
-                <br />
-                {this.RenderItems()}
+                <UpdateContent />
               </ul>
             </ScrollWrapper>
           </ScrollOuterWrapper>
---
+++ src/component/UpdateContent.js
@@ -0,0 +1,101 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const UpdateItemWrapper = styled.div`
+  margin-right: 50px;
+`;
+const DisableListDot = styled.li`
+  list-style-type: none;
+`;
+const HrWrapper = styled.hr`
+  border-width: 1px 0px 0px 0px;
+  border-style: solid;
+  border-color: #cccccc;
+  height: 1px;
+`;
+const UpdateHrWrapper = styled.div`
+  color: '#ffffff';
+  border-width: 2.5px 0 0 0;
+`;
+class UpdateContent extends React.Component {
+  render() {
+    const UpdateItems = [
+      { value: 'TOEICスコア更新', date: '2020.6.8' },
+      { value: 'インターン更新', date: '2020.4.10' },
+      { value: 'Aboutページ更新', date: '2020.11.18' },
+      {
+        value: <a href="https://baplisca.herokuapp.com/">React適用に伴うサイト移動</a>,
+        date: '2020.11.1',
+      },
+      {
+        value: (
+          <a
+            href="https://www.youtube.com/channel/UCUEUc2teEFnYEXG-MtEncrA/playlists"
+            rel="noreferrer noopener"
+            target="_blank">
+            TadanoAction3DXGameのYoutube追加
+          </a>
+        ),
+        date: '2020.10.16',
+      },
+      {
+        value: (
+          <a
+            href="https://baplisca.hatenablog.com/entry/intern/nssol"
+            rel="noreferrer noopener"
+            target="_blank">
+            NSSOLインターンのブログリンク追加
+          </a>
+        ),
+        date: '2020.10.12',
+      },
+      {
+        value: (
+          <a
+            href="https://baplisca.hatenablog.com/entry/supporterz-1on1"
+            rel="noreferrer noopener"
+            target="_blank">
+            エンジニア1on1に参加しました
+          </a>
+        ),
+        date: '2020.10.5',
+      },
+      {
+        value: (
+          <a
+            href="https://baplisca.hatenablog.com/entry/supporterz-geekten"
+            rel="noreferrer noopener"
+            target="_blank">
+            技育展に登壇しました
+          </a>
+        ),
+        date: '2020.9.26',
+      },
+      { value: 'Youtube埋め込み・デザイン変更', date: '2020.9.13' },
+      { value: 'フッターに各種リンク追加・ナビゲーションバー変更', date: '2020.9.4' },
+      { value: 'インターンのブログリンク追加・フッター作成', date: '2020.9.3' },
+      { value: 'インターン項目・受賞追加', date: '2020.8.23' },
+      { value: '競プロライブラリ更新', date: '2020.7.22' },
+      { value: 'フォント変更 Google Map埋め込み', date: '2020.6.11' },
+      { value: 'スマホ対応, 画像圧縮 (PNG→JPEG)', date: '2020.5.29' },
+      { value: '競プロライブラリ追加', date: '2020.5.28' },
+      { value: 'HP作成', date: '2020.5.24' },
+    ];
+    return UpdateItems.map((_item) => {
+      return (
+        <React.Fragment>
+          <UpdateItemWrapper>
+            <DisableListDot>{_item.date}</DisableListDot>
+            <DisableListDot>{_item.value}</DisableListDot>
+            <UpdateHrWrapper>
+              <HrWrapper />
+            </UpdateHrWrapper>
+          </UpdateItemWrapper>
+          <br />
+        </React.Fragment>
+      );
+    });
+  }
+}
+
+export default UpdateContent;
