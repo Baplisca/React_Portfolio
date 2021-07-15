8e466bdf60e1ed5aa1187b84dd4e9960ce764aae
“Baplisca” <yokotako0327-goki@outlook.jp>
2021-07-15 15:24:11 +0900

    recreate nav to set language choice

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,68 +1,58 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import styled from 'styled-components';
 import { NavLink } from 'react-router-dom';
 
 import './i18n';
 import { withTranslation } from 'react-i18next';
 
-const NavWrapper = styled.div`
-  position: relative; /* for IE11 */
+const HeaderWrapper = styled.div`
   position: sticky;
   top: 0px;
   z-index: 100;
 
   background-color: #343a40;
-`;
-const H2Wrapper = styled.h2`
-  font-size: 20px;
-  margin: 0px 0px 0px 0px;
-  padding: 4px 0px 4px 7px;
-  border-bottom: 2px solid #999999;
-`;
-const NavInner = styled.nav``;
-const NavUl = styled.ul`
-  display: table;
-  margin: 0 auto;
-  padding: 0;
-  width: 50%;
-  text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 10px;
 
-  li {
-    display: table-cell;
-    min-width: 20vw;
-    color: #fff;
-  }
-  li a {
-    display: block;
-    width: 100%;
-    padding: 10px 0;
-    text-decoration: none;
-    color: #fff;
-    font-weight: bold;
-    transition: 0s;
-  }
-  li:hover {
-    background-color: #555;
-    border: none;
-    border-radius: 10px;
-  }
+  height: 50px;
+`;
+const HeaderMenu = styled.ul`
+  margin: 0;
+  padding: 0;
+  font-size: 18px;
+  list-style: none;
+  display: flex;
 `;
 
 /* https://stackoverflow.com/questions/50208306/use-active-state-from-react-router-in-styled-components */
 const MyNavLink = styled(NavLink)`
+  color: #888;
+  text-decoration: none;
+  padding: 15px;
   &.${(props) => props.activeClassName} {
-    background-color: #555;
-    border: none;
-    border-radius: 10px;
+    // background-color: #555;
+    // border: none;
+    // border-radius: 10px;
+    color: #fff;
+  }
+  :hover {
+    color: #fff;
   }
 `;
-class Navbar extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isEng: false,
-    };
+const LngWrapper = styled.li`
+  :hover {
+    color: #fff;
   }
+  cursor: pointer;
+  color: ${(props) => (props.language === 'en' ? '#fff' : '#888')};
+`;
+const GapWrapper = styled.li`
+  margin: 0 10px;
+  color: #fff;
+`;
+class Navbar extends Component {
   render() {
     const { t, i18n } = this.props;
     const changeLanguage = (lng) => {
@@ -70,31 +60,33 @@ class Navbar extends Component {
     };
     return (
       <React.Fragment>
-        <NavWrapper>
-          <H2Wrapper>
-            <NavInner>
-              <NavUl>
-                <li>
-                  <MyNavLink activeClassName="active" exact to="/">
-                    {t('About')}
-                  </MyNavLink>
-                </li>
-                <li>
-                  <MyNavLink activeClassName="active" to="/work">
-                    Work
-                  </MyNavLink>
-                </li>
-                <li>
-                  <MyNavLink activeClassName="active" to="/contact">
-                    Contact
-                  </MyNavLink>
-                </li>
-                <li onClick={() => changeLanguage('ja')}>日</li>
-                <li onClick={() => changeLanguage('en')}>A</li>
-              </NavUl>
-            </NavInner>
-          </H2Wrapper>
-        </NavWrapper>
+        <HeaderWrapper>
+          <div></div>
+          <HeaderMenu>
+            <li>
+              <MyNavLink activeClassName="active" exact to="/">
+                {t('About')}
+              </MyNavLink>
+            </li>
+            <li>
+              <MyNavLink activeClassName="active" to="/work">
+                Work
+              </MyNavLink>
+            </li>
+            <li>
+              <MyNavLink activeClassName="active" to="/contact">
+                Contact
+              </MyNavLink>
+            </li>
+            <GapWrapper>|</GapWrapper>
+            <LngWrapper language={!i18n.language} onClick={() => changeLanguage('ja')}>
+              あ
+            </LngWrapper>
+            <LngWrapper language={i18n.language} onClick={() => changeLanguage('en')}>
+              A
+            </LngWrapper>
+          </HeaderMenu>
+        </HeaderWrapper>
       </React.Fragment>
     );
   }
